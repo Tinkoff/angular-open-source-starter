257676c68394e2dbce307367fa384d411bea0147
a.inkin <a.inkin@tinkoff.ru>
2019-09-12 16:31:15 +0300

    fix(build): fix build issues and bump dependencies to get rid of vulnerabilities from devDependencies

--- projects/demo/static.paths.ts
+++ projects/demo/static.paths.ts
@@ -1,3 +1,5 @@
 import {appRoutes} from './src/app/app.routes';
 
-export const PRERENDERED_ROUTES: ReadonlyArray<string> = appRoutes.map(route => route.path || '');
+export const PRERENDERED_ROUTES: ReadonlyArray<string> = appRoutes
+    .map(route => route.path || '')
+    .filter(path => path && path !== '**');
