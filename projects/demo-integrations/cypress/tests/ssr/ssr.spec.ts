428d2dabd44e7e7d5c32a0f5b624ba65fd92520d
splincode <omaxphp@yandex.ru>
2022-04-06 16:32:44 +0300

    ci: fix prerender and ssr e2e tests

--- projects/demo-integrations/cypress/tests/ssr/ssr.spec.ts
+++ projects/demo-integrations/cypress/tests/ssr/ssr.spec.ts
@@ -1,18 +1,16 @@
 describe('Server side rendering', () => {
+    const baseUrl: string = Cypress.config('baseUrl') ?? '/';
+
     it('should serve statics and favicon.ico', () => {
-        cy.request('http://localhost:4000/favicon.ico')
-            .its('status')
-            .should('equal', 200);
+        cy.request(`${baseUrl}/favicon.ico`).its('status').should('equal', 200);
     });
 
     it('should successfully render static url', () => {
-        cy.request('http://localhost:4000')
-            .its('body')
-            .should('include', 'This is a static route');
+        cy.request(baseUrl).its('body').should('include', 'This is a static route');
     });
 
     it('should successfully render lazy url', () => {
-        cy.request('http://localhost:4000/lazy')
+        cy.request(`${baseUrl}/lazy`)
             .its('body')
             .should('include', 'This is a lazy route');
     });
