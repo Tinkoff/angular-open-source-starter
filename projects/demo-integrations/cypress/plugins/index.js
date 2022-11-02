4491ce6980eef85e5a44f35d3e9c68e5a7ff16c3
Barsukov Nikita <nikita.s.barsukov@gmail.com>
2022-11-02 18:28:50 +0300

    chore: update `Cypress` to v10

--- projects/demo-integrations/cypress/plugins/index.js
+++ projects/demo-integrations/cypress/plugins/index.js
@@ -1,9 +1,11 @@
-const {viewportWidth, viewportHeight} = require('./../../cypress.json');
+const {CYPRESS_CONFIG} = require('./../../cypress.config');
 
 module.exports = on => {
     on('before:browser:launch', (browser, launchOptions) => {
         if (browser.name === 'chrome') {
-            launchOptions.args.push(`--window-size=${viewportWidth},${viewportHeight}`);
+            launchOptions.args.push(
+                `--window-size=${CYPRESS_CONFIG.viewportWidth},${CYPRESS_CONFIG.viewportHeight}`,
+            );
             launchOptions.args.push('--force-device-scale-factor=2');
             launchOptions.args.push('--high-dpi-support=1');
             launchOptions.args.push('--disable-dev-shm-usage');
